838d144c8f65ec2fc9c6b31862067bc2afd21296
Aaron <blakugb@gmail.com>
2026-04-08 17:25:05 -0500

    Defining the function get summary with two parameters that are given by the user, then based on the url create respective url to bring the JSON object out and find the summary of that chapter chosen and book

--- accessApi.py
+++ accessApi.py
@@ -1,1 +1,10 @@
 import requests
+def get_summary(book, chapter):
+    base_url = 'https://openscriptureapi.org/api/scriptures/v1/lds/en/volume/bookofmormon/'
+    # TODO: Create a URL string that will access the API for the given book and chapter
+    # HINT: The URL should be in the format f"{base_url}{book.lower()}/{chapter}"
+    url_spec = f"{base_url}{book.lower()}/{chapter}"
+    # HINT: Use the requests.get() method to access the API and return the JSON data
+    data = requests.get(url_spec)
+    # HINT: Extract the summary from the JSON data and return it
+    print(data.json()["chapter"]["summary"])
