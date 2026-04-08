fa428168c74c70e7db49097143a03323735c4b9a
Aaron <blakugb@gmail.com>
2026-04-08 17:26:03 -0500

    Defining the user interface that interact with the user . Giving the welcome and displaying all the books to choose, input prompt for the user asking the book and the chapter . Applying try and expect to handle errors like the book could not exist . Moreover the function use a while loop to provoke a exit in the case the user does not use the program once it is used by asking to the user if user wanna get an another different summary

--- accessApi.py
+++ accessApi.py
@@ -8,3 +8,28 @@ def get_summary(book, chapter):
     data = requests.get(url_spec)
     # HINT: Extract the summary from the JSON data and return it
     print(data.json()["chapter"]["summary"])
+
+def run_summary_tool():
+    # Print a welcome message as shown in the example. "Welcome to the Book of Mormon Summary Tool!"
+    print("Welcome to the Book of Mormon Summary Tool!")
+    # Use a while loop to allow the user to input a book and chapter
+    while True:
+    # Ask the user for the book and chapter they would like to view a summary of
+        books_menu = {"_id":"bookofmormon","books":[{"_id":"1nephi","title":"1 Nephi"},{"_id":"2nephi","title":"2 Nephi"},{"_id":"jacob","title":"Jacob"},{"_id":"enos","title":"Enos"},{"_id":"jarom","title":"Jarom"},{"_id":"omni","title":"Omni"},{"_id":"wordsofmormon","title":"Words of Mormon"},{"_id":"mosiah","title":"Mosiah"},{"_id":"alma","title":"Alma"},{"_id":"helaman","title":"Helaman"},{"_id":"3nephi","title":"3 Nephi"},{"_id":"4nephi","title":"4 Nephi"},{"_id":"mormon","title":"Mormon"},{"_id":"ether","title":"Ether"},{"_id":"moroni","title":"Moroni"}],"title":"Book of Mormon","titleOfficial":"The Book of Mormon","titleShort":"BoM"}
+        print("List of the books")
+        for i in range(len(books_menu["books"])):
+            print(f"({i}) {books_menu["books"][i]["title"]}")
+        try:
+            book = input("Choose a Book: ").capitalize()
+            chapter = int(input("Choose a chapter: "))
+    # Use the get_summary() function to retrieve the summary and print it
+            get_summary(book,chapter)
+    # If the book or chapter is invalid, catch the KeyError and print an error message
+        except KeyError:
+            print("Please enter a exist book")
+    # Ask the user if they would like to view another summary
+        ask = input("Would do you like another summary?(y/n)")
+    # If the user does not want to view another summary, print "Thank you for using Book of Mormon Summary Tool!"
+        if ask == "n":
+            print("\"Thank you for using Book for Mormon Summary Tool!\"")
+    # do not forget to finish or break the loop
